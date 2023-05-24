7caff03c055e00f7c458998efae88aa1ef018e75
mandarinca <myler4ik@gmail.com>
2023-05-24 11:43:53 +0300

    test final1

---
+++ src/modules/data.js
@@ -0,0 +1,12 @@
+
+export const refs = {
+
+pixabayKey: '36594105-8544a69bb4c5d0c0362bb11bf',
+formEl: document.querySelector('.search-form'),
+galleryEl: document.querySelector('.gallery'),
+//const moreBtnEl = document.querySelector('.load-more');
+PER_PAGE: 4,
+lightbox: new SimpleLightbox('.gallery a')
+
+
+}
---
+++ src/modules/functions.js
@@ -0,0 +1,30 @@
+export default function createMarkup(imgArr) {
+  
+  imgArr.forEach(el => {
+      refs.galleryEl.insertAdjacentHTML(
+
+          'beforeEnd',
+          `<div class="photo-card">
+
+          <a class="photo" href="${el.webformatURL}">
+          <img src="${el.webformatURL}" alt="${el.tags}" loading="lazy" width='480' height='320'/>
+          </a>
+
+          <div class="info">
+            <p class="info-item">
+              <b>Likes: ${el.likes}</b>
+            </p>
+            <p class="info-item">
+              <b>Views: ${el.views}</b>
+            </p>
+            <p class="info-item">
+              <b>Comments: ${el.comments}</b>
+            </p>
+            <p class="info-item">
+              <b>Downloads: ${el.downloads}</b>
+            </p>
+          </div>
+        </div>`
+      );
+    });
+}
